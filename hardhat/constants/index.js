aaa107c49e5fa647d6f21d93ca148ef94d24905e
overthestream <njhnjh02@naver.com>
2022-09-15 00:41:20 +0900

    complete

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x86c13d2634225d050ff2d93045c5136660ae4920";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
