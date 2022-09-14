e6ce112983930af7b5ce3fcf370996a71af574bd
overthestream <njhnjh02@naver.com>
2022-09-15 00:44:26 +0900

    change next to goerli

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -241,13 +241,6 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the Rinkeby network, let them know and throw an error
-    const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
-    }
-
     if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
